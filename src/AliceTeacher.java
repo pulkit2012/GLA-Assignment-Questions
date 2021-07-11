a561ab3dc2fc5967f3c626c3ed25ce6d8ed99279
Pulkit Varshney <pulkitvarshney2012@gmail.com>
2021-07-11 23:10:55 +0530

    Codes for GLA-Recursion-assignment Added

--- src/AliceTeacher.java
+++ src/AliceTeacher.java
@@ -1,28 +1,20 @@
 import java.util.Scanner;
 
 public class AliceTeacher {
-    static int count = 0;
-    public static void classAssignment(int n, String ans) {
-        if (ans.length() == n) {
-            count++;
-            return;
-        }
-        classAssignment(n, ans + 'a');
-        if (ans.length() == 0 || ans.charAt(ans.length() - 1) != 'b') {
-            classAssignment(n, ans + 'b');
-        }
+    public static int assign(int n){
+        if(n==1 || n==2)
+            return(n+1);
+        else
+            return(assign(n-1)+assign(n-2));
     }
-
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        in.nextLine();
-        while (num > 0) {
-            int val = in.nextInt();
-            classAssignment(val, "");
-            System.out.println("#" + val + " : " + count);
-            count = 0;
-            num--;
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=1;i<=t;i++){
+            int k=sc.nextInt();
+            int j=assign(k);
+            System.out.println("#"+i+" : "+j);
         }
     }
 }
+//Warning-------------------this code is copied!!!!!!!!!!!!!
